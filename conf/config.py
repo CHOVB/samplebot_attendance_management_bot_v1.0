dd579b0f96ceeb4a043352337d5e6268b02a3390
CHOVB <97274082+CHOVB@users.noreply.github.com>
2022-06-14 20:40:46 +0900

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -36,11 +36,11 @@ SERVER_ID = "p12e5.serviceaccount@robotcom"
 SECRET_KEY_NAME = "private_20220614203543.key"
 
 # LOCAL ADDRESS
-LOCAL_ADDRESS = "https://robotncomchatbot.herokuapp.com/"
+LOCAL_ADDRESS = "https://robotncomchatbot2.herokuapp.com/"
 
 # DB config
-DB_HOST = "ec2-xxx-xxx-xxx-xxx.compute-1.amazonaws.com"
-DB_NAME = "x3mdxxxxsg5nx"
-DB_USER = "xsozxxxxvksikx"
+DB_HOST = "ec2-54-147-33-38.compute-1.amazonaws.com"
+DB_NAME = "d1p00p8hjhihaa"
+DB_USER = "bquunvqokkjggf"
 DB_PORT = "5432"
-DB_PASSWORD = "xxxxxb3ff80c85d331da880f4b3e0xxxxxxxxxxxd552d64d128317653ce0xxxx"
+DB_PASSWORD = "ecf737acb500458c1cb76d82395db9801b822e7dab0cb285830bc4847b0a85d6"
